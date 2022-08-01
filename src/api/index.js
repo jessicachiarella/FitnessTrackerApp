1b9462884c251ff72106269b85db5acf157bb655
Carlos Nava <chnava@me.com>
2022-08-01 14:26:37 -0500

    working on login person function

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,39 @@
-const API_URL =  "https://fitnesstrac-kr.herokuapp.com/api/";
+const API_URL = "https://fitnesstrac-kr.herokuapp.com/api/";
 
 export async function RegisterPerson(event) {
-    const registerUsername = event.target[0].value;
-    const registerPassword = event.target[1].value;
-    const response = await fetch(`${API_URL}/users/register`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user: {
-          username: registerUsername,
-          password: registerPassword,
-        },
-      }),
-    });
-  
-    const result = await response.json();
-    return result;
-  }
+  const registerUsername = event.target[0].value;
+  const registerPassword = event.target[1].value;
+
+  const response = await fetch(`${API_URL}/users/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: registerUsername,
+      password: registerPassword,
+    }),
+  });
+
+  const result = await response.json();
+  return result;
+}
+
+export async function LoginPerson(event) {
+  const loginUsername = event.target[0].value;
+  const loginPassword = event.target[1].value;
+
+  const response = await fetch(`${API_URL}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: loginUsername,
+      password: loginPassword,
+    }),
+  });
+  const result = await response.json();
+  const token = result.data.token;
+  return token;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
 export {default as App} from './App'
+export {default as Register} from './users/Register'
+export {default as Header} from './Header'
+export {default as Login} from './users/Login'
+
